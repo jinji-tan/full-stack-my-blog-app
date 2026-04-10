31394b0b805c7854db8167dd42ec212789957c7c
Louis Tan <louistan600@gmail.com>
2026-04-10 19:21:38 +0800

    fix css

--- frontend/my-blog-app/src/components/PostCard.jsx
+++ frontend/my-blog-app/src/components/PostCard.jsx
@@ -124,7 +124,7 @@ const PostCard = ({ post, onPostDeleted, onPostUpdated }) => {
                         <textarea
                             value={editedContent}
                             onChange={(e) => setEditedContent(e.target.value)}
-                            className="w-full px-3 py-2 text-slate-600 leading-relaxed border border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/20 min-h-[150px]"
+                            className="w-full px-3 py-2 text-slate-600 leading-relaxed border border-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/20 min-h-37.5"
                         />
                         <div className="flex justify-end gap-3 mt-4">
                             <button
